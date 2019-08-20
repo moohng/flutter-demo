25bdb2290be620595c6907ad47f89b4a59584b14
Kevin <moohng@126.com>
2019-08-20 23:23:29 +0800

    对话框

--- lib/pages/home/HomePage.dart
+++ lib/pages/home/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,12 +12,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark));
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        middle: Text('首页'),
-        backgroundColor: CupertinoColors.activeBlue,
+        middle: Text('首页', style: TextStyle(color: CupertinoColors.white),),
+        backgroundColor: Colors.lightBlue,
       ),
-      child: Center(child: Text('首页'),),
+      child: Container(
+        child: GestureDetector(
+          child: Text('首页'),
+          onTap: () {
+            showCupertinoDialog(
+              context: context,
+              builder: (context) {
+                return CupertinoAlertDialog(
+                  title: Text('提示'),
+                  actions: <Widget>[
+                    CupertinoButton(child: Text('确定'), onPressed: () => Navigator.of(context).pop(),),
+                    CupertinoButton(child: Text('取消'), onPressed: () => Navigator.of(context).pop(),),
+                  ],
+                  content: Text('这是一段内容，这是一段内容，这是一段内容这是一段内容这是一段内容，这是一段内容这是一段内容。这是一段内容？'),
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
