298f84a2be2d4dc326a7664b2a2c3d30c6a67d40
Kevin <moohng@126.com>
2019-08-22 00:07:43 +0800

    地图

--- lib/pages/map/MapPage.dart
+++ lib/pages/map/MapPage.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/cupertino.dart';
+// import 'package:amap_base/amap_base.dart';
 
 class MapPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MapPageState();
   }
 }
 
 class _MapPageState extends State<MapPage> {
+
+  // AMapController _aMapController;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('地图'),
       ),
-      child: Center(child: Text('地图'),),
+      // child: AMapView(
+      //   amapOptions: AMapOptions(
+      //     zoomGesturesEnabled: true,
+      //   ),
+      // ),
+      child: Center(child: Text('ddd'),),
     );
   }
 }
