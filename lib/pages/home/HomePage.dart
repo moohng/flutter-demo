f24fc570de1baa817a471cf02ed637b4d7d10274
mohong <moohng@126.com>
2019-08-21 09:46:20 +0800

    暂存

--- lib/pages/home/HomePage.dart
+++ lib/pages/home/HomePage.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_demo/utils/HttpUtil.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +10,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData();
+  }
+
+  void fetchData() async {
+    Response response = await HttpUtil.get('s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=flutter%20网络请求&oq=flutter%2520%25E7%25BD%2591%25E7%25BB%259C%25E8%25AF%25B7%25E6%25B1%2582&rsv_pq=f3ca83c10016a9d6&rsv_t=f576pKqz%2BbCUtwtNoQbGhJ%2Fpq%2BeLPj2CIaJ5FrO1N%2FAC%2FVEiM5YT8ZhITdI&rqlang=cn&rsv_enter=0&rsv_dl=tb');
+    print(response);
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
--- lib/utils/HttpUtil.dart
+++ lib/utils/HttpUtil.dart
@@ -6,9 +6,20 @@ class HttpUtil {
   // 全局定义
   static Dio dio;
 
+  // 默认 options
+  static const String BASE_URL = 'http://www.baidu.com/';
+  static const int CONNECT_TIMEOUT = 20000;
+  static const int RECEIVE_TIMEOUT = 5000;
+
   static Dio createInstance() {
     if (dio == null) {
-      dio = Dio();
+      // 初始设置
+      BaseOptions options = BaseOptions(
+        baseUrl: BASE_URL,
+        connectTimeout: CONNECT_TIMEOUT,
+        receiveTimeout: RECEIVE_TIMEOUT,
+      );
+      dio = Dio(options);
       // 请求拦截器
       dio.interceptors.add(InterceptorsWrapper(
         onRequest: (RequestOptions options) {
@@ -23,6 +34,11 @@ class HttpUtil {
       ));
     }
     return dio;
+  }
+
+  static Future get(String path, { Options options, Map<String, dynamic> queryParameters, CancelToken cancelToken, void Function(int, int) onReceiveProgress }) {
+    Dio dio = createInstance();
+    return dio.get(path, options: options, queryParameters: queryParameters, cancelToken: cancelToken, onReceiveProgress: onReceiveProgress);
   }
 
   static clear() {
